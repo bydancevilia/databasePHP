de540d347fa25c158d2d9dc92a9cec3179225fe6
Ivan <nikualert@mail.ru>
2021-12-14 14:22:21 +0300

    three

--- dobavlenie.php
+++ dobavlenie.php
@@ -2,14 +2,16 @@
 require_once 'connect.php';
 ?>
 <form action="dobavlenie.php" method="GET">
-Адрес помещения:<input type="integer" name="Adres"><br><br>
-Количество комнат:<input type="integer" name="Komnati"><br><br>
-Этаж:<input type="integer" name="Etag"><br><br>
+<br>
+Адрес помещения:<input type="text" name="Adres"><br><br>
+Количество комнат:<input type="text" name="Komnati"><br><br>
+Этаж:<input type="text" name="Etag"><br><br>
 Балкон:<input type="text" name="Balkon"><br><br>
-Стоимость в сутки:<input type="integer" name="Stoimost"><br><br>
+Стоимость в сутки:<input type="text" name="Stoimost"><br><br>
 Владелец:<select name="Vladelec">
 <?php
 $result=mysqli_query($link,"SELECT
+  vladelec.id_vladelec,
   vladelec.Last_Name
 FROM dogofor
   INNER JOIN client
@@ -21,18 +23,18 @@ FROM dogofor
 $rows=mysqli_fetch_all($result,MYSQLI_ASSOC);
  foreach($rows as $row)
 {
-        echo"<option>".($row ['Last_Name']."<br>");
+        echo"<option>".($row ['id_vladelec']."<br>");
+
 }
 ?>
 </select>
-<br><br>
 <input type="submit" name="submit" value="Добавить"><br>
+
 </form>
 <?php
-
 if($_GET['submit'])
 {
 	$result=mysqli_query($link,"INSERT HIGH_PRIORITY INTO   pomeshenie (id_pomeshenia, `kol-vo komnat`, adres, etag, balkon, id_vladelec, `stoimost za sutki`)
-  VALUES (0, '$_GET[Komnati]', '$_GET[Adres]', '$_GET[Etag]', '$_GET[Balkon]', '$_GET[Vladelec]', '$_GET[Stoimost]'); ");
+  VALUES (0, $_GET[Komnati], '$_GET[Adres]', $_GET[Etag], '$_GET[Balkon]', $_GET[Vladelec], $_GET[Stoimost]); ");
 }
 ?>
